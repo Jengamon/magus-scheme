cedc2633c1fb2741e3a1efff444618a3ad6ca009
Jengamon <bob.hostern@gmail.com>
2025-03-22 15:31:08 -0700

    Implement prototype for `assq` and `assv`
    - typecheck for association list is not implemented

--- src/stdlib/scheme_base.scm
+++ src/stdlib/scheme_base.scm
@@ -3,6 +3,8 @@
 ; We only have the relevant native modules available
 (import (scheme base))
 (export list not map memq memv)
+; Sketch functions (to be removed once implemented properly)
+(export assq assv)
 (begin
   (define (list . in) in)
   (define (not x) (if x #f #t))
@@ -18,6 +20,19 @@
                   lst
                   (memq-iter a (cdr lst)))))
       (memq-iter x lst))
+  (define (assq x alist)
+      ; TODO This is a sketch b/c typechecking on "alist" is required
+      ; (list of lists where each member list has length == 2)
+      ; (b/c 'It is an error if alist (for “association list”) is not a list of pairs.'
+      ; in the report.) We *could* check it (if we support raise-continuable), but it would require
+      ; a separate iteration that isn't really worth it compared to just implementing it in Rust.
+      (define (assq-iter k ascl)
+          (if (null? ascl)
+              #f
+              (if (eq? k (caar ascl))
+                  (car ascl)
+                  (assq-iter k (cdr ascl)))))
+      (assq-iter x alist))
   (define (memv x lst)
       (define (memv-iter a lst)
           (if (null? lst)
@@ -26,6 +41,19 @@
                   lst
                   (memv-iter a (cdr lst)))))
       (memv-iter x lst))
+  (define (assv x alist)
+      ; TODO This is a sketch b/c typechecking on "alist" is required
+      ; (list of lists where each member list has length == 2)
+      ; (b/c 'It is an error if alist (for “association list”) is not a list of pairs.'
+      ; in the report.) We *could* check it (if we support raise-continuable), but it would require
+      ; a separate iteration that isn't really worth it compared to just implementing it in Rust.
+      (define (assv-iter k ascl)
+          (if (null? ascl)
+              #f
+              (if (eqv? k (caar ascl))
+                  (car ascl)
+                  (assv-iter k (cdr ascl)))))
+      (assv-iter x alist))
   ; TODO member, which uses equal (does a length check, and should error if rest is too long,
   ; so waiting on impls equal?, raise)
   (define-syntax when
